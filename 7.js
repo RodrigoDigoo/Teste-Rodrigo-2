68e3ba0287d21327a64e2141b8e9d782b0f70661
RodrigoDigoo <117772757+RodrigoDigoo@users.noreply.github.com>
2022-11-08 16:38:35 -0300

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,8 @@
+// #7
+random = Math.floor(Math.random() * 43) + 1; // 1 até 43
+for(var i=1; i <= 44; i++){
+  
+  console.log(i);
+  
+  if(i == random){
+    console.log("Estou entre "+ (i) +" e " + (
